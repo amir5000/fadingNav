c9a3212aa5cfe2fed81368ecdfce79f366aae775
amir5000 <amirarafat1@gmail.com>
2015-07-06 01:11:45 -0600

    added refresh rate as an option, also added fading option on hover.

--- fadingNav.js
+++ fadingNav.js
@@ -5,13 +5,20 @@
 		var $t, scrollAmount, alphaValue, alphaCalc, alphaCalcChange, itemHeight;
 		this.each(function() {
 			$t = $(this);
-			$(window).on('load scroll',function(){
+			$(window).on('load scroll', function(){
 				scroll			= true;
 				scrollAmount	= $(window).scrollTop();
 				alphaCalc		= opts.bkgColor;
 				itemHeight		= opts.itemHeight.height();
 				alphaValue		= opts.alpha;
 				alphaCalcChange	= scrollAmount / itemHeight;
+
+				$t.on('mouseenter', function() {
+					$t.css('background-color', 'rgba(' + opts.bkgColor + ', 1)');
+				});
+				$t.on('mouseleave', function() {
+					$t.css('background-color', 'rgba(' + opts.bkgColor + ',' + alphaCalcChange + ')');
+				});
 				if (scroll == true && scrollAmount < itemHeight) {
 					setTimeout(function() {				
 					    if (scrollAmount === 0 || alphaValue >= alphaCalcChange ) {
@@ -21,8 +28,10 @@
 					        alphaCalc = 'rgba(' + opts.bkgColor + ',' + alphaCalcChange + ')';
 					        $t.css('background-color', alphaCalc);
 					    }			
-					}, 50);
+					}, opts.intervalSpeed);
 					scroll = false;
+				} else if (scroll == true && scrollAmount >= itemHeight) {
+					$t.css('background-color', 'rgba(' + opts.bkgColor + ', 1)');
 				}
 			});
 			return this;
@@ -32,7 +41,9 @@
 	$.fn.fadingNav.defaults = {
 	    bkgColor: '0, 0, 0',
 	    alpha: 0,
-	    itemHeight : $(".banner")
+	    itemHeight: $(".banner"),
+	    refreshRate: 50,
+	    inverse: 1
 	};
 }( jQuery ));
 
