98638669d10199d260483e38f65b8d688bdea2d9
amir5000 <amirarafat1@gmail.com>
2015-07-12 16:00:20 -0600

    added touch events

--- fadingNav.js
+++ fadingNav.js
@@ -5,56 +5,57 @@
 		var $t, scrollAmount, alphaValue, alphaCalc, alphaCalcChange, itemHeight, mouseIsEntered;
 		this.each(function() {
 			$t = $(this);
-			$(window).on('load scroll touchmove', function(){
-				scroll			= true;
-				mouseIsEntered	= false;
-				scrollAmount	= $(window).scrollTop();
-				alphaCalc		= opts.bkgColor;
-				itemHeight		= opts.itemHeight.height();
-				alphaValue		= opts.alpha;
-				alphaCalcChange	= scrollAmount / itemHeight;
-				if (opts.fadeOnHover === true) {
-					$t.on('mouseenter', function() {
-						mouseIsEntered = true;
-						$t.css('background-color', 'rgba(' + opts.bkgColor + ', 1)').addClass(opts.fadeOnHoverClass);
-					});
-					
-					$t.on('mouseleave', function() {
-						if (scrollAmount === 0 || alphaValue >= alphaCalcChange ) {
-							$t.css('background-color', 'rgba(' + opts.bkgColor + ',' + alphaValue + ')').stop().delay(opts.removeOnHoverClassDelay).queue(function(){
-                    			$t.removeClass(opts.fadeOnHoverClass).dequeue();
-                    		});
-                    	} else if (scrollAmount !== 0 && alphaValue < alphaCalcChange) {
-							$t.css('background-color', 'rgba(' + opts.bkgColor + ',' + alphaCalcChange + ')').stop().delay(opts.removeOnHoverClassDelay).queue(function(){
-	                    		$t.removeClass(opts.fadeOnHoverClass).dequeue();
-	                    	});
-						}
-						mouseIsEntered = false;
-					});
-					
-				}
-				if (scroll === true && scrollAmount < itemHeight && mouseIsEntered !== true) {
-					setTimeout(function() {				
-					    if (scrollAmount === 0 || alphaValue >= alphaCalcChange ) {
-					        alphaCalc = 'rgba(' + opts.bkgColor + ', ' + alphaValue + ')';
-					        $t.css('background-color', alphaCalc);
-					    } else if (alphaValue < alphaCalcChange) {
-					        alphaCalc = 'rgba(' + opts.bkgColor + ',' + alphaCalcChange + ')';
-					        $t.css('background-color', alphaCalc);
-					    }
-					    if (opts.enableBreakPoint === true) {
-						    if (alphaCalcChange >= opts.breakPointValue) {
-						    	$t.addClass(opts.breakPointClass);
-						    } else if (alphaCalcChange < opts.breakPointValue) {
-						    	$t.removeClass(opts.breakPointClass);
+				$(window).on('load scroll', function(){
+					scroll			= true;
+					mouseIsEntered	= false;
+					scrollAmount	= $(window).scrollTop();
+					alphaCalc		= opts.bkgColor;
+					itemHeight		= opts.itemHeight.height();
+					alphaValue		= opts.alpha;
+					alphaCalcChange	= scrollAmount / itemHeight;
+					if (opts.fadeOnHover === true) {
+						$t.on('mouseenter touchmove touchstart', function() {
+							mouseIsEntered = true;
+							$t.css('background-color', 'rgba(' + opts.bkgColor + ', 1)').addClass(opts.fadeOnHoverClass);
+						});
+						
+						$t.on('mouseleave touchend', function() {
+							if (scrollAmount === 0 || alphaValue >= alphaCalcChange ) {
+								$t.css('background-color', 'rgba(' + opts.bkgColor + ',' + alphaValue + ')').stop().delay(opts.removeOnHoverClassDelay).queue(function(){
+	                    			$t.removeClass(opts.fadeOnHoverClass).dequeue();
+	                    		});
+	                    	} else if (scrollAmount !== 0 && alphaValue < alphaCalcChange) {
+								$t.css('background-color', 'rgba(' + opts.bkgColor + ',' + alphaCalcChange + ')').stop().delay(opts.removeOnHoverClassDelay).queue(function(){
+		                    		$t.removeClass(opts.fadeOnHoverClass).dequeue();
+		                    	});
+							}
+							mouseIsEntered = false;
+						});
+						
+					}
+					if (scroll === true && scrollAmount < itemHeight && mouseIsEntered !== true) {
+						setTimeout(function() {				
+						    if (scrollAmount === 0 || alphaValue >= alphaCalcChange ) {
+						        alphaCalc = 'rgba(' + opts.bkgColor + ', ' + alphaValue + ')';
+						        $t.css('background-color', alphaCalc);
+						    } else if (alphaValue < alphaCalcChange) {
+						        alphaCalc = 'rgba(' + opts.bkgColor + ',' + alphaCalcChange + ')';
+						        $t.css('background-color', alphaCalc);
 						    }
-						}
-					}, opts.refreshRate);
-					scroll = false;
-				} else if (scroll === true && scrollAmount >= itemHeight && mouseIsEntered !== true) {
-					$t.css('background-color', 'rgba(' + opts.bkgColor + ', 1)');
-				}
-			});
+						    if (opts.enableBreakPoint === true) {
+							    if (alphaCalcChange >= opts.breakPointValue) {
+							    	$t.addClass(opts.breakPointClass);
+							    } else if (alphaCalcChange < opts.breakPointValue) {
+							    	$t.removeClass(opts.breakPointClass);
+							    }
+							}
+						}, opts.refreshRate);
+						scroll = false;
+					} else if (scroll === true && scrollAmount >= itemHeight && mouseIsEntered !== true) {
+						$t.css('background-color', 'rgba(' + opts.bkgColor + ', 1)');
+					}
+				});
+			
 			return this;
 		});
 	};
